d95adaab4716c2f012041567b4348ff8d1e84eeb
DSlundro <dragan.savic95@outlook.it>
2022-05-03 15:51:11 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -26,5 +26,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  a{
+    text-decoration: none;
+  }
 }
 </style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,17 +1,18 @@
 <template>
     <header>
         <div class="container">
-            <div class="row py-3">
+            <div class="row">
 
-                <div class="col-3">
+                <div class="col-3 d-flex align-items-center">
                     <img src="@/assets/img/dc-logo.png" alt="">
                 </div>
 
-                <div class="col-9 d-flex justify-content-end align-items-center">
-                    <nav class="navbar navbar-dark bg-dark">
-                        <a href="#">CHARACTERS</a>
+                <div class="col-9 d-flex justify-content-end align-items-center" >
+                    <nav class="navbar navbar-light py-0 px-3" v-for="(nome, index) in navEelement" :key="index">
+                        <a href="#" class="py-5">{{nome.name}}</a>
                     </nav>
                 </div>
+                
             </div>
         </div>
     </header>
@@ -20,9 +21,56 @@
 <script>
 export default {
     name: 'HeaderComponent',
+    data(){
+        return{
+            navEelement:
+            [
+        {
+            name: 'characters',
+        },
+        {
+            name: 'comics',
+        },
+        {
+            name: 'movies',
+        },
+        {
+            name: 'tv',
+        },
+        {
+            name: 'games',
+        },
+        {
+            name: 'collectibles',
+        },
+        {
+            name: 'videos',
+        },
+        {
+            name: 'fans',
+        },
+        {
+            name: 'news',
+        },
+        {
+            name: 'shop',
+        },
+    ]
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-
+    header{
+        nav{
+            a{
+                color: #0282F9;
+                text-transform: uppercase;
+                &:hover{
+                    border-bottom: 3px solid #0282F9;
+                }
+            }
+        }
+    }
 </style>
